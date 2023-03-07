d402541cfe2d41aca97cf93b9d30209144cdedbc
Leonardo Palavecino <64588981+leo43161@users.noreply.github.com>
2023-03-07 12:05:00 -0300

    Se creo el dropzone para deudas falta crear las funciones para formatear los datos

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -12,7 +12,7 @@ export default function Header() {
                     <Navbar.Brand href="/">Turismo Tucuman</Navbar.Brand>
                     <Nav className="me-auto">
                         <Nav.Link href="/pagos" active={router.asPath === "/pagos"}>Pagos</Nav.Link>
-                        <Nav.Link href="/carga" active={router.asPath === "/carga"}>Cargar pagos</Nav.Link>
+                        <Nav.Link href="/carga" active={router.asPath === "/carga"}>Cargar</Nav.Link>
                         <Nav.Link href="/provee" active={router.asPath === "/provee"}>Imprimir Comprobantes</Nav.Link>
                     </Nav>
                 </Container>
--- src/pages/carga/index.jsx
+++ src/pages/carga/index.jsx
@@ -5,19 +5,35 @@ import DropZone from '../../components/Dropzone';
 import leerArchivo from '../../helpers/pagosHelpers';
 
 export default function Carga() {
-    const [pagoFile, setPagoFile] = useState(null)
+    const [pagoFile, setPagoFile] = useState(null);
+    const [deudasFile, setDeudasFile] = useState(null);
     return (
         <div className="mt-4">
             <div className="container d-flex justify-content-center">
-                <div className="col-8">
+                <div className="col-6 me-3">
                     <Card className="p-4 shadow">
-                        <h2 className="text-center mb-3">Inserte el archivo txt para cargar los pagos</h2>
+                        <h1 className="text-center">Pagos</h1>
+                        <h3 className="text-center mb-3">Inserte el archivo txt</h3>
                         <div className="d-flex justify-content-center mb-3" style={{ height: "300px" }}>
                             <DropZone setState={setPagoFile}></DropZone>
                         </div>
                         <div className="d-flex justify-content-center">
                             <Button variant="success" onClick={() => leerArchivo(pagoFile)} disabled={!pagoFile}>
                                 <span className="">Subir pagos</span>
+                            </Button>
+                        </div>
+                    </Card>
+                </div>
+                <div className="col-6">
+                    <Card className="p-4 shadow">
+                    <h1 className="text-center">Deudas</h1>
+                        <h3 className="text-center mb-3">Inserte el archivo txt</h3>
+                        <div className="d-flex justify-content-center mb-3" style={{ height: "300px" }}>
+                            <DropZone setState={setDeudasFile}></DropZone>
+                        </div>
+                        <div className="d-flex justify-content-center">
+                            <Button variant="success" onClick={() => leerArchivo(deudasFile)} disabled={!deudasFile}>
+                                <span className="">Subir deudas</span>
                             </Button>
                         </div>
                     </Card>
