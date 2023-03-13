9b49c3b150f6ba15df287c130daf5b171ced2d19
Leonardo Palavecino <64588981+leo43161@users.noreply.github.com>
2023-03-13 10:56:12 -0300

    Se formateo los pagos a subir, falta empezar a subir los proovedores y los pagos

--- src/helpers/pagosHelpers.js
+++ src/helpers/pagosHelpers.js
@@ -7,34 +7,66 @@ const codRetVerif = {
     402: "RET. IMP. GANANCIAS - PROVEEDORES Y CONTRATIS",
     409: "S.G.P. SIST. PENS. JUB. RETENC. PERSONAL"
 }
-function formatearTxt(txt) {
-    const contenido = txt;
+
+const formatDeudas = {
+    1: "nCuenta",
+    2: "nombre",
+    28: "retencion",
+    32: "nOrden",
+    34: "fecha",
+    35: "nCompromiso",
+    37: "codRet",
+    38: "recursoEstado",
+    39: "tipoRet",
+    45: "importeRet",
+}
+const FORMAT_PAGOS = {
+    0: "nCuenta",
+    1: "nombre",
+    11: "nOrden",
+    14: "fechaP",
+    28: "codExpEatt",
+    32: "cuit",
+    33: "razonSocial",
+    36: "descGastos",
+    39: "cbuProv",
+    41: "fechafact",
+    42: "tipoFact",
+    43: "factura1",
+    44: "factura2",
+}
+function formatearPagos(_pagos) {
+    console.log(_pagos);
+    let pagos = [];
+    _pagos.forEach((pago) => {
+        let _pago = {};
+        for (const key in pago) {
+            if (FORMAT_PAGOS[key]) {
+                _pago[FORMAT_PAGOS[key]] = pago[key];
+            }
+        }
+        _pago = formatearPago(_pago);
+        pagos.push(_pago);
+    });
+    console.log(pagos);
+    subirPagos(pagos);
+}
+
+const formatearTxt = (contenido, txt) => {
     const arrayData = contenido.split(/\r\n|\r|\n/, -1);
     const listaPagos = [];
     arrayData.forEach((element) => {
-        let pago = {};
-        pago.ctaEmisora = element.substring(2, 10);
-        pago.fechaEmision = `${element.substring(10, 14)}-${element.substring(14, 16)}-${element.substring(16, 18)}`;
-        pago.ejercicio = element.substring(10, 14);
-        pago.cheque = element.substring(18, 26);
-        pago.cuit = parseInt(element.substring(51, 62));
-        pago.beneficiarios = element.substring(62, 122);
-        pago.tipoComp = element.substring(144, 146);
-        pago.ordenPago = element.substring(146, 152);
-        pago.compPago = element.substring(152, 158);
-        pago.monto = parseInt(element.substring(158, 171));
-        pago.monto = pago.monto / 100;
-        pago.entidad = element.substring(171, 173);
-        pago.codRet = element.substring(183, 186);
-        pago.tipoGasto = element.substring(271, 274);
-        pago.fechaAcred = `${element.substring(246, 250)}-${element.substring(250, 252)}-${element.substring(252, 254)}`;
+        const pago = element.split("	");
         listaPagos.push(pago);
     })
+    listaPagos.pop();
     /* console.log(arrayData); */
-    subirPagos(listaPagos);
+    console.log(listaPagos);
+    if (txt === 1) formatearPagos(listaPagos);
+    /* if (txt === 2) writeTable(listaPagos, formatDeudas); */
 }
 
-export default function leerArchivo(file) {
+export default function leerArchivo(file, txt) {
     var archivo = file;
     if (!archivo) {
         return;
@@ -42,12 +74,14 @@ export default function leerArchivo(file) {
     var lector = new FileReader();
     lector.onload = function (e) {
         const contenido = e.target.result;
-        return formatearTxt(contenido);
+        return formatearTxt(contenido, txt);
     };
     lector.readAsText(archivo);
 }
 
+
 const subirPagos = async (pagos = []) => {
+    console.log(pagos);
     const countPagos = {
         proveedores: [],
         ordenesDePago: [],
@@ -58,11 +92,16 @@ const subirPagos = async (pagos = []) => {
         pagosDetalleOmitidos: [],
         cantidad: 0
     }
+    return;
     /* Se aplica la funcion para realizar la filtracion general */
     const _pagos = filterPagos(pagos);
+
     console.log(_pagos.length);
+
     const findPago = _pagos.filter(e => e.ordenPago === "114879");
+
     console.log(findPago);
+
     _pagos.forEach(async (pago, index) => {
         /* console.log(codRetVerif[pago.codRet]); */
         console.log(pago.ctaEmisora === "71975050" ? pago : false);
@@ -112,10 +151,24 @@ const subirPagos = async (pagos = []) => {
     console.log(countPagos);
 }
 
+const subirDedudas = () => {
+
+}
+
 const filterPagos = (pagos = []) => {
     /* Se aplica la filtracion a la cuenta de pagos del ente EATT - Pagos segun su nro de cuenta */
     let filterPagos = pagos.filter(({ ctaEmisora }) => ctaEmisora !== "71975050");
     filterPagos = filterPagos.filter(({ ctaEmisora }) => ctaEmisora !== "71975050");
     console.log(filterPagos);
     return filterPagos;
+}
+const formatearPago = (pago) => {
+    let _pago = pago;
+    _pago.codExpEatt = pago.codExpEatt.slice(2, -1).replace('460   ', '-');
+    _pago.fechaP = pago.fechaP.slice(0, 4) + "-" + pago.fechaP.slice(4, 6) + "-" + pago.fechaP.slice(6, 8);
+    _pago.nombre = pago.nombre.trim();
+    _pago.fechafact = pago.fechafact.slice(0, 4) + "-" + pago.fechafact.slice(4, 6) + "-" + pago.fechafact.slice(6, 8);
+    _pago.tipoFact = pago.tipoFact.replace('FACT ', '');
+    _pago.nFactura = `${pago.factura1}-${pago.factura2}`;
+    return _pago;
 }
--- src/pages/carga/index.jsx
+++ src/pages/carga/index.jsx
@@ -18,7 +18,7 @@ export default function Carga() {
                             <DropZone setState={setPagoFile}></DropZone>
                         </div>
                         <div className="d-flex justify-content-center">
-                            <Button variant="success" onClick={() => leerArchivo(pagoFile)} disabled={!pagoFile}>
+                            <Button variant="success" onClick={() => leerArchivo(pagoFile, 1)} disabled={!pagoFile}>
                                 <span className="">Subir pagos</span>
                             </Button>
                         </div>
@@ -26,7 +26,7 @@ export default function Carga() {
                 </div>
                 <div className="col-6">
                     <Card className="p-4 shadow">
-                    <h1 className="text-center">Deudas</h1>
+                        <h1 className="text-center">Deudas</h1>
                         <h3 className="text-center mb-3">Inserte el archivo txt</h3>
                         <div className="d-flex justify-content-center mb-3" style={{ height: "300px" }}>
                             <DropZone setState={setDeudasFile}></DropZone>
